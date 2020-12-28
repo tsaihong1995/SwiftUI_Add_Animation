18a70a1bd142c93b264a047d2e9e4176e98bdd16
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-28 12:48:25 -0500

    Initial Commit

---
+++ SwiftUI_Add_Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Add_Animation
+//
+//  Created by Hung-Chun Tsai on 2020-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Add_Animation/SwiftUI_Add_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Add_AnimationApp.swift
+//  SwiftUI_Add_Animation
+//
+//  Created by Hung-Chun Tsai on 2020-12-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Add_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
